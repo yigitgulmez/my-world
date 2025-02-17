184046cfcc2910e8c1b5fb731e1c6eae706c8dc2
Yiğit <ygulmez873@gmail.com>
2025-02-17 22:54:45 +0300

    Update linklist.tsx

--- src/utils/linklist.tsx
+++ src/utils/linklist.tsx
@@ -1,14 +1,13 @@
 import { ReactNode } from 'react';
-import { AiOutlineGithub, AiOutlineLink } from 'react-icons/ai';
+import { AiOutlineGithub } from 'react-icons/ai';
 import { RxVercelLogo } from 'react-icons/rx';
 const linkLists: Record<string, { href: string; icon: ReactNode }[]> = {
   // 'my-world': [
   //   { href: 'https://Example.com', icon: <AiOutlineLink/> },
   // ],
   'my-world': [
-    { href: 'https://yeniasd.com', icon: <AiOutlineLink/> },
-    { href: 'https://yeniasd.com', icon: <AiOutlineGithub/> },
     { href: 'https://yeniasd.com', icon: <RxVercelLogo/> },
+    { href: 'https://github.com/yigitgulmez/my_worl', icon: <AiOutlineGithub/> },
   ],
 };
 
