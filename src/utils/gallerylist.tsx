58a647d606ff194cc4fb6c59f9821c24d8ad59e9
Yiğit <ygulmez873@gmail.com>
2025-02-17 21:01:41 +0300

    Finish

--- src/utils/gallerylist.tsx
+++ src/utils/gallerylist.tsx
@@ -1,4 +1,12 @@
 const list = [
+  { img: '/images/gallery/Realme C25S (1).webp', alt: 'Realme C25S (1)', },
+  { img: '/images/gallery/Realme C25S (2).webp', alt: 'Realme C25S (2)', },
+  { img: '/images/gallery/Realme C25S (3).webp', alt: 'Realme C25S (3)', },
+  { img: '/images/gallery/Realme C25S (4).webp', alt: 'Realme C25S (4)', },
+  { img: '/images/gallery/Realme C25S (5).webp', alt: 'Realme C25S (5)', },
+  { img: '/images/gallery/Realme C25S (6).webp', alt: 'Realme C25S (6)', },
+  { img: '/images/gallery/Realme C25S (7).webp', alt: 'Realme C25S (7)', },
+  { img: '/images/gallery/Realme C25S (8).webp', alt: 'Realme C25S (8)', },
   // { img: '/images/gallery/Example.webp', alt: 'Image', },
 ];
 
--- src/utils/linklist.tsx
+++ src/utils/linklist.tsx
@@ -1,8 +1,15 @@
 import { ReactNode } from 'react';
+import { AiOutlineGithub, AiOutlineLink } from 'react-icons/ai';
+import { RxVercelLogo } from 'react-icons/rx';
 const linkLists: Record<string, { href: string; icon: ReactNode }[]> = {
   // 'my-world': [
   //   { href: 'https://Example.com', icon: <AiOutlineLink/> },
   // ],
+  'my-world': [
+    { href: 'https://yeniasd.com', icon: <AiOutlineLink/> },
+    { href: 'https://yeniasd.com', icon: <AiOutlineGithub/> },
+    { href: 'https://yeniasd.com', icon: <RxVercelLogo/> },
+  ],
 };
 
 export default linkLists;
