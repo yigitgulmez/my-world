3637f9b37ff4a6484bdbf7ffaac2c16d91922044
Yiğit Gülmez <ygulmez873@gmail.com>
2025-02-26 00:39:41 +0300

    Update linklist.tsx

--- src/utils/linklist.tsx
+++ src/utils/linklist.tsx
@@ -1,12 +1,10 @@
 import { ReactNode } from 'react';
 import { AiOutlineGithub } from 'react-icons/ai';
-import { RxVercelLogo } from 'react-icons/rx';
 const linkLists: Record<string, { href: string; icon: ReactNode }[]> = {
   // 'my_world': [
   //   { href: 'https://Example.com', icon: <AiOutlineLink/> },
   // ],
   'my_world': [
-    { href: 'https://my-world-sable.vercel.app', icon: <RxVercelLogo/> },
     { href: 'https://github.com/yigitgulmez/my_world/', icon: <AiOutlineGithub/> },
   ],
 };
